fbf2460dfbb29459b5098111854ae16e63ae63b8
SoaresKevin <kevin.soares@unifebe.edu.br>
2019-08-20 00:01:01 -0300

    informar os valores inicio;

---
+++ main.py
@@ -0,0 +1,8 @@
+valores = []
+num_valores = int(input("Digite o numero de valores que deseja inserir:"))
+
+for x in range(0,num_valores):
+    valor = int(input("Digite o valor a ser trabalhado:"))
+    valores.append(valor)
+
+    
