dfd14911c26aad1044dae7744515474e271a5899
SoaresKevin <kevin.soares@unifebe.edu.br>
2019-08-25 16:37:13 -0300

    fix;

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import util
+
 valores = []
 opcao = 0
+linhas_num = int(input("Quantas linhas de dados deseja trabalhar?"))
 
-itens = input("Digite os itens que deseja trabalhar separados por (/):")
-valores = itens.split('/')
-for item in valores:
-    print(item)
+for x in range(0,linhas_num): 
+    itens = input("Digite os itens que deseja trabalhar separados por (/):")
+    valores += itens.split('/')
+valores = map(int, valores)
+valores_sort = sorted(valores)
+
+for x in teste:
+    print(x)
+
