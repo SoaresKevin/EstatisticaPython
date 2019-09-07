2e629d953d947143a3d8bcfbb8caf3d2bb76e42e
SoaresKevin <kevin.soares@unifebe.edu.br>
2019-09-07 19:06:44 -0300

    fix;

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ arquivo = open(arquivo, "w")
 
 valores = []
 val_sorted = []
-stage = "Dev"
+stage = "dev"
 media = 0
 
 if stage == "Production":
@@ -23,7 +23,7 @@ if stage == "Production":
         itens = input("Digite os itens que deseja trabalhar separados por (/):")
         valores += itens.split('/')
 else:
-    itens = "80/85/90"
+    itens = "400/300/330/300/330/330/430/400/380/550/350/450/280/350/450/530/500/350/420/400/350/550/450/330/380/500/480/450/450/280/450/250/250/300/280/430/420/300/520/350/380/400/450/400/480/520/450/400/450/450/300/330/380/420/420/430/480/500/480/430"
     valores = itens.split('/')
 
 valores = map(float, valores)
@@ -35,7 +35,7 @@ msg = ""
 for x in val_sorted:
     msg += str(x)+"/" 
     i += 1
-    if i == 12:
+    if i == 10:
         i = 0   
         print_valores.append(msg)
         msg = ""
@@ -80,8 +80,26 @@ for x in list:
 print("-------------------------------------")
 arquivo.write('-------------------------------------\n')
 
-msg = u"Medidas de variabilidade sao " + str(util.calc_medidas_variabilidade(val_sorted)) +'\n'
+msg = u"Variancia " + str(util.calc_variancia(val_sorted)) +'\n'
 print(msg) 
 arquivo.write(msg)
+
+print("-------------------------------------")
+arquivo.write('-------------------------------------\n')
+
+
+msg = u"Desvio Padrao " + str(util.calc_devio_padrao(val_sorted)) +'\n'
+print(msg) 
+arquivo.write(msg)
+
+print("-------------------------------------")
+arquivo.write('-------------------------------------\n')
+
+
+msg = u"Coeficiente de Variacao " + str(util.calc_coeficiente_variacao(val_sorted)) +'% \n'
+print(msg) 
+arquivo.write(msg)
+
+
 
 arquivo.close()
--- util.py
+++ util.py
@@ -5,6 +5,8 @@ import statistics
 
 def calc_mediana(valores):
     posicao = (len(valores) +1)/2 
+    print("Posicao:",posicao)
+    print("Tamanho:",len(valores))
     if Decimal(posicao) % 1 == 0:
         return valores[int(posicao)]
     else:
@@ -12,6 +14,8 @@ def calc_mediana(valores):
         posi_menor = floor(posicao)
         print(posi_maior)
         print(posi_menor)
+        print(valores[posi_maior-1])
+        print(valores[posi_menor-1])
         return (( valores[posi_maior-1] + valores[posi_menor-1])/2)
 
 def list_moda(val_sorted):
@@ -22,9 +26,29 @@ def list_moda(val_sorted):
     return list
     
 
-def calc_medidas_variabilidade(val_sorted):
+def calc_variancia(val_sorted):
+    media = round(statistics.mean(val_sorted), 6) 
+    total = 0
+    print("--Variancia Totais--")
+    for x in set(val_sorted): 
+        total += round((val_sorted.count(x) * (( x - media )**2)),6)
+        print(str(x),"-",str(val_sorted.count(x)),"-",(round((val_sorted.count(x) * (( x - media )**2)),6)))
+    print("--Variancia Totais--")
+    return round((total/len(val_sorted)),6)
+
+def calc_coeficiente_variacao(val_sorted):
+    desvio_padrao = calc_devio_padrao(val_sorted)
+    media = round(statistics.mean(val_sorted),6)
+
+    return round( ((desvio_padrao/media) * 100),6)
+    
+
+def calc_devio_padrao(val_sorted):
     media = round(statistics.mean(val_sorted), 6) 
     total = 0
     for x in set(val_sorted): 
         total += round((val_sorted.count(x) * (( x - media )**2)),6)
-    return round((total/len(val_sorted)),6)
+    resultado = round((total/len(val_sorted)),6)
+    raizQ = resultado ** (1/2)
+    return raizQ
+
